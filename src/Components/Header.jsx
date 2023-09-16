86df6c36695f247b37cc3e773ff517e3f1666e6c
Danny Jung <dkmjung1@gmail.com>
2023-09-16 11:14:04 -0700

    add header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,23 @@
+// Header.js
+
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Playa Playa</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
