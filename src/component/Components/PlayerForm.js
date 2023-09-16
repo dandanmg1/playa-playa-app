69ccaceccd4e27ee832c1207d55a192370b54aad
Lester Blake <lstr.mail@gmail.com>
2023-09-16 14:08:35 -0400

    first commit

---
+++ src/component/Components/PlayerForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function playerForm () {
+   <div className={""}>
+    <form className={""}>
+                <label>
+                    <h3 className="">Title Your Video</h3>
+                    <input
+                        className={""}
+                        type={"text"}
+                        name={""}
+                        placeholder={"Add a Title to your Video..."}
+                    />
+                </label>
+                <label>
+                <h3 className="uploadPage__subtitle">Add a Video Description</h3>
+                    <input
+                        className={"uploadPage__form__input"}
+                        type={"text"}
+                        name={"Add A Video Description"}
+                        placeholder={"Add a Description to your Video..."}
+                    />
+                </label>
+                </form>
+   </div>
+}
+
+export default playerForm
